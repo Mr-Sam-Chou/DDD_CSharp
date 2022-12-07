b2e71985f212fab285c87da7f72478e0ecd3a0e2
Sam Chou <sam78621@hotmail.com>
2022-12-07 15:53:03 +0800

    test keyword testing

--- lab1_unittest/PrimeService.Tests/UnitTest1.cs
+++ lab1_unittest/PrimeService.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System;
 namespace PrimeService.Tests;
 
 [TestFixture]
@@ -6,16 +7,36 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        Console.WriteLine("Ready!!");
     }
-
+    [TearDown]
+    public void StoreData()
+    {
+        Console.WriteLine("Reverted!!");
+    }
+    [OneTimeSetUp]
+    public void init()
+    {
+         //Console.WriteLine("onetime start action");
+        TestContext.Progress.WriteLine("onetime start action");
+    }
+    [OneTimeTearDown]
+    public void finalize()
+    {
+        //Console.WriteLine("onetime finalize action");
+        TestContext.Progress.WriteLine("onetime finalize action");
+    }
     [Test]
     public void Test1()
     {
+        Console.WriteLine("Test test1");
         Assert.Pass();
     }
     [Test]
+    [Ignore("to do....")]
     public void Test2()
     {
+        Console.WriteLine("Test test2");
         Assert.Fail("Test Fail");
     }
 }
