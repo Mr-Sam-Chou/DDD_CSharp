2ba6052591643f025d5f45b6ce0c33498f6d1f10
Sam Chou <sam78621@hotmail.com>
2022-12-08 17:08:39 +0800

    bind data

--- lab4_ef_core/Program.cs
+++ lab4_ef_core/Program.cs
@@ -3,7 +3,7 @@
 using Model;
 using var db = new BlogContext();
 Console.WriteLine($"資料庫的路徑是:{db.DbPath}");
-if (db.Blogs.Count() == 0)
+if (db.Blogs!.Count() == 0)
 {
     db.Add(new Blog { Uri = "http://www.uuu.com.tw/" });
     db.Add(new Blog { Uri = "http://www.tibame.com/" });
@@ -14,7 +14,7 @@ else
     Console.WriteLine("修改一下資料");
     var blog = db.Blogs!.OrderBy(b => b.BlogId).Last();
     blog.Uri = "https://www.cwb.gov.tw";
-    if (db.Blogs.Count() == 0)
+    if (db.Blogs!.Count() == 0)
     {
         blog.Posts.Add(new Post { Title = "urgent", Content = "Type is coming" });
         blog.Posts.Add(new Post { Title = "urgent", Content = "Earthquake last night" });
@@ -24,8 +24,13 @@ else
 
 db.SaveChanges();
 Console.WriteLine("將資料讀回");
-var blogs = db.Blogs.OrderBy(b => b.BlogId);
+var blogs = db.Blogs!.OrderBy(b => b.BlogId);
 foreach (var b in blogs)
 {
     Console.WriteLine($"{b.BlogId},{b.Uri}");
+    var posts = db.Posts!.Where(p => b.BlogId == p.BlogId);
+    foreach (var p in posts)
+    {
+        Console.WriteLine($"..{p.Title}/{p.Content}");
+    }
 }
