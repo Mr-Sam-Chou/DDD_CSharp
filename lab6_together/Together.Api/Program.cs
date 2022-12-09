5ef6718d3d631f753408e924d25e45107665aea3
Sam Chou <sam78621@hotmail.com>
2022-12-09 16:51:36 +0800

    use DI new

--- lab6_together/Together.Api/Program.cs
+++ lab6_together/Together.Api/Program.cs
@@ -1,8 +1,10 @@
+using Together.Application;
 using Together.Application.Services;
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
-    builder.Services.AddScoped<IEventService, EventService>();
+    //builder.Services.AddScoped<IEventService, EventService>();
+    builder.Services.AddApplication();
 }
 
 var app = builder.Build();
